df054495aeed526fed2bfc45d371ea858787e39e
蔡冉修改 <cairan@cairandeMacBook-Pro.local>
2018-05-03 11:03:54 +0800

    打包压缩文件分离

--- webpack.config.js
+++ webpack.config.js
@@ -18,26 +18,34 @@ module.exports={
                     options: {
                         presets: ['es2015']
                     }
-                },
+                }, 
                 exclude: "/node_modules/"
             },
             {
                 test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
+                // use: [
+                //     {
+                //         loader: "style-loader"
+                //     },
+                //     {
+                //         loader: "css-loader"
+                //     }
+                // ]
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader"]
+                })
             }
         ]
     },
     plugins: [
+        // 依据一个简单的index.html模板，生成一个自动引用你打包后的JS文件的新index.html
         new HtmlWebpackPlugin(),
+        // 为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
         new webpack.optimize.OccurrenceOrderPlugin(),
+        // 压缩 不用配置
         new webpack.optimize.UglifyJsPlugin(),
-        new ExtractTextPlugin('common.css',{allChunks:true})
+        // 分离 use: ExtractTextPlugin.extract
+        new ExtractTextPlugin('./css/common.css')
     ]
 }
